101e408275e169854ba88cbffd3d6e728a1c6fb4
Ron Nash <ronamazing@gmail.com>
2023-02-01 16:11:33 -0500

    motion.py works now. Sensitivity needs refinement.

--- ledstrip/motion.py
+++ ledstrip/motion.py
@@ -14,29 +14,29 @@ pir = MotionSensor(17)
 
 while True:
     pir.wait_for_motion()
-        
-    #while waitingstill():
-    for x in (8, 9, 10, 11):
-        pca.channels[x].duty_cycle=0x7FFF
-        pca.channels[x].duty_cycle=0xFFFF
-        pca.channels[x].duty_cycle=0xFFFF
-        pca.channels[x].duty_cycle=0xFFFF
-        time.sleep(.0625)
-        pca.channels[x].duty_cycle=0xFFFF
-        pca.channels[x].duty_cycle=0x7FFF
-        pca.channels[x].duty_cycle=0xFFFF
-        pca.channels[x].duty_cycle=0xFFFF
-        time.sleep(.0625)
-        pca.channels[x].duty_cycle=0xFFFF
-        pca.channels[x].duty_cycle=0xFFFF
-        pca.channels[x].duty_cycle=0x7FFF
-        pca.channels[x].duty_cycle=0xFFFF
-        time.sleep(.0625)
-        pca.channels[x].duty_cycle=0xFFFF
-        pca.channels[x].duty_cycle=0xFFFF
-        pca.channels[x].duty_cycle=0xFFFF
-        pca.channels[x].duty_cycle=0x7FFF
-        time.sleep(.0625)
     print("You moved")
+    pca.channels[0].duty_cycle=0xFFFF
+    pca.channels[1].duty_cycle=0xFFFF
+    pca.channels[2].duty_cycle=0xFFFF
+    pca.channels[3].duty_cycle=0xFFFF
+    pca.channels[4].duty_cycle=0xFFFF
+    pca.channels[5].duty_cycle=0xFFFF
+    pca.channels[6].duty_cycle=0xFFFF
+    pca.channels[7].duty_cycle=0xFFFF
+    pca.channels[12].duty_cycle=0xFFFF
+    pca.channels[13].duty_cycle=0xFFFF
+    pca.channels[14].duty_cycle=0xFFFF
+    pca.channels[15].duty_cycle=0xFFFF
     pir.wait_for_no_motion()
-
+    pca.channels[0].duty_cycle=0x0
+    pca.channels[1].duty_cycle=0x0
+    pca.channels[2].duty_cycle=0x0
+    pca.channels[3].duty_cycle=0x0
+    pca.channels[4].duty_cycle=0x0
+    pca.channels[5].duty_cycle=0x0
+    pca.channels[6].duty_cycle=0x0
+    pca.channels[7].duty_cycle=0x0
+    pca.channels[12].duty_cycle=0x0
+    pca.channels[13].duty_cycle=0x0
+    pca.channels[14].duty_cycle=0x0
+    pca.channels[15].duty_cycle=0x0
